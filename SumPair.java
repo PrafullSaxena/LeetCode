63b987660fb94a34068de70634758978a93e09a8
Prafull Saxena <ps.prafullsaxena0@gmail.com>
2022-01-11 12:46:17 +0530

    Update SumPair.java

--- SumPair.java
+++ SumPair.java
@@ -16,7 +16,18 @@ import java.util.*;
 
 public class SumPair {
 
-    public List<List<Integer>> optimized(int[] nums, int target){
+    public static void main(String[] args) {
+        int[] list = {4, 5, 7, 11, 9, 13, 8, 12};
+        int target = 20;
+
+        System.out.println("Answer from BruteForce - ");
+        BruteForce(list, target).forEach(System.out::println);
+        System.out.println("Answers from Optimized - ");
+        optimized(list, target).forEach(System.out::println);
+
+    }
+
+    public static List<List<Integer>> optimized(int[] nums, int target){
 
         List<List<Integer>> resultSet = new ArrayList<>();
         List<Integer> previousElement = new ArrayList<>();
@@ -25,7 +36,7 @@ public class SumPair {
 
             if(nums[i] > target) continue;
             int key = target - nums[i];
-            
+
             if(previousElement.contains(key)){
                 resultSet.add(List.of(nums[i], key));
             }else previousElement.add(nums[i]);
@@ -34,15 +45,15 @@ public class SumPair {
     }
 
 
-    public List<List<Integer>> BruteForce(int[] nums, int target){
-        
+    public static List<List<Integer>> BruteForce(int[] nums, int target){
+
         List<List<Integer>> resultSet = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
-                
+
                 if(nums[i] > target) continue;
-                
+
                 if(nums[i] + nums[j] == target) {
                     resultSet.add(List.of(nums[i], nums[j]));
                 }
@@ -52,3 +63,4 @@ public class SumPair {
     }
 
 }
+
